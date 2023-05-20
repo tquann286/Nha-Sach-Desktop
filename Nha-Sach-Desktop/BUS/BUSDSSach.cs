a4f60199eb2562981c3665e485f992adf6b5b18d
Truong Nhut Huy <tnhuy1092003@gmail.com>
2023-05-20 19:06:43 +0700

    sach

---
+++ Nha-Sach-Desktop/BUS/BUSDSSach.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nha_Sach_Desktop.DAO;
+using Nha_Sach_Desktop.DTO;
+
+namespace Nha_Sach_Desktop.BUS
+{
+    internal class BUSDSSach
+    {
+        public static List<DTODSSach> GetDSSach()
+        {
+            return DAODSSach.GetDSSach();   
+        }
+    }
+}
---
+++ Nha-Sach-Desktop/DAO/DAODSSach.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nha_Sach_Desktop.DTO;
+
+namespace Nha_Sach_Desktop.DAO
+{
+    internal class DAODSSach
+    {
+        public static List<DTODSSach> GetDSSach()
+        {
+            List<DTODSSach> DsSach = new List<DTODSSach>();
+            using (NhaSachBatOnDataContext dbMain = new NhaSachBatOnDataContext())
+            {
+                var query = from p in dbMain.SACHes select p;
+
+                foreach (var row in query)
+                {
+                    DTODSSach sach = new DTODSSach();
+
+                    sach.MaSach = row.masach;
+                    sach.TenSach = row.tensach;
+                    sach.TheLoai = row.theloai;
+                    sach.TacGia = row.tacgia;
+                    sach.DonGia = row.dongia.ToString("###,###,###' 'VNĐ");
+                    sach.Luongton = row.toncuoi.ToString();
+                    DsSach.Add(sach);
+                }
+            }
+            return DsSach;
+        }
+    }
+}
