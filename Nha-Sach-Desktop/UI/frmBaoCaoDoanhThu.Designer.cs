c9d10bbbfae3667112fa7e0ae090c1f6b5c99fa6
tquann286 <quantrung286@gmail.com>
2023-05-20 09:30:17 +0700

    fix layout

--- Nha-Sach-Desktop/UI/frmBaoCaoDoanhThu.Designer.cs
+++ Nha-Sach-Desktop/UI/frmBaoCaoDoanhThu.Designer.cs
@@ -53,7 +53,7 @@
             this.dgvBaoCaoNo.Margin = new System.Windows.Forms.Padding(4);
             this.dgvBaoCaoNo.Name = "dgvBaoCaoNo";
             this.dgvBaoCaoNo.RowHeadersWidth = 51;
-            this.dgvBaoCaoNo.Size = new System.Drawing.Size(993, 218);
+            this.dgvBaoCaoNo.Size = new System.Drawing.Size(697, 218);
             this.dgvBaoCaoNo.TabIndex = 55;
             // 
             // btnThoat
@@ -63,10 +63,10 @@
             this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
             this.btnThoat.Image = ((System.Drawing.Image)(resources.GetObject("btnThoat.Image")));
             this.btnThoat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnThoat.Location = new System.Drawing.Point(867, 441);
+            this.btnThoat.Location = new System.Drawing.Point(780, 381);
             this.btnThoat.Margin = new System.Windows.Forms.Padding(4);
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(164, 52);
+            this.btnThoat.Size = new System.Drawing.Size(182, 52);
             this.btnThoat.TabIndex = 54;
             this.btnThoat.Text = "  THOÁT  ";
             this.btnThoat.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -101,10 +101,10 @@
             this.btnXuat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnXuat.Image = ((System.Drawing.Image)(resources.GetObject("btnXuat.Image")));
             this.btnXuat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnXuat.Location = new System.Drawing.Point(636, 441);
+            this.btnXuat.Location = new System.Drawing.Point(780, 302);
             this.btnXuat.Margin = new System.Windows.Forms.Padding(4);
             this.btnXuat.Name = "btnXuat";
-            this.btnXuat.Size = new System.Drawing.Size(203, 52);
+            this.btnXuat.Size = new System.Drawing.Size(182, 52);
             this.btnXuat.TabIndex = 50;
             this.btnXuat.Text = "IN BÁO CÁO";
             this.btnXuat.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -152,10 +152,10 @@
             this.btnChiTiet.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnChiTiet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnChiTiet.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(124)))), ((int)(((byte)(35)))));
-            this.btnChiTiet.Location = new System.Drawing.Point(457, 442);
+            this.btnChiTiet.Location = new System.Drawing.Point(780, 215);
             this.btnChiTiet.Margin = new System.Windows.Forms.Padding(4);
             this.btnChiTiet.Name = "btnChiTiet";
-            this.btnChiTiet.Size = new System.Drawing.Size(151, 55);
+            this.btnChiTiet.Size = new System.Drawing.Size(182, 55);
             this.btnChiTiet.TabIndex = 56;
             this.btnChiTiet.Text = "CHI TIẾT";
             this.btnChiTiet.UseVisualStyleBackColor = false;
@@ -164,7 +164,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1054, 501);
+            this.ClientSize = new System.Drawing.Size(1054, 462);
             this.Controls.Add(this.btnChiTiet);
             this.Controls.Add(this.dgvBaoCaoNo);
             this.Controls.Add(this.btnThoat);
