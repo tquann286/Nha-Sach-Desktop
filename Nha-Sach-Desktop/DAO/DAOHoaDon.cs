b89c473422926c9a6841de628724a3ac690ba4e1
TanPun <tanpun890@gmail.com>
2023-05-20 15:06:35 +0700

    hd

---
+++ Nha-Sach-Desktop/DAO/DAOHoaDon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nha_Sach_Desktop.DTO;
+
+namespace Nha_Sach_Desktop.DAO
+{
+    public class DAOHoaDon
+    {
+        public bool InsertHoaDon(DTOHoaDon dTOHoaDon)
+        {
+            try
+            {
+                NhaSachBatOnDataContext dbMain = new NhaSachBatOnDataContext();
+                HOADON hd = new HOADON();
+                hd.mahd = dTOHoaDon.Mahd;
+                hd.makh = dTOHoaDon.Makh;
+                hd.tenkh = dTOHoaDon.Tenkh;
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
--- Nha-Sach-Desktop/UI/frmHoaDon.cs
+++ Nha-Sach-Desktop/UI/frmHoaDon.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,23 @@ namespace Nha_Sach_Desktop.UI
         public frmHoaDon()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void frmHoaDon_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-1TEJ4NIL;Initial Catalog=BatOnBookStore;Integrated Security=True");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Select * from HOADON", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            dataGridView1.DataSource = ds.Tables[0];
         }
     }
 }
