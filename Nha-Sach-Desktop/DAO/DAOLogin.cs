3182fe78ca9f87879c759e5df4e7806c8ae953eb
TanPun <tanpun890@gmail.com>
2023-05-20 10:01:03 +0700

    login dao

---
+++ Nha-Sach-Desktop/DAO/DAOLogin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nha_Sach_Desktop.DAO;
+using System.Data.SqlClient;
+using Nha_Sach_Desktop.DTO;
+using System.Linq;
+
+namespace Nha_Sach_Desktop.DAO
+{
+    class DAOLogin
+    {
+        public static bool KiemTraTaiKhoan(DTOLogin user)
+        {
+            dbMainDataContext dbMain = new dbMainDataContext();
+
+            var svt = (from a in dbMain.Accounts
+                       where a.UserName == user.taiKoan && a.PassWord == user.matKhau
+                       select a);
+            if (svt.Count() > 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ Nha-Sach-Desktop/DTO/DTOLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nha_Sach_Desktop.DTO
+{
+    public class DTOLogin
+    {
+        public string taiKoan { get; set; }
+        public string matKhau { get; set; }
+    }
+}
