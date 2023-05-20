81e852f723196cc6c3c3dd1d5ed8d39fdfdc7e7e
nguyenphuoc <hoangphuochp1511@gmail.com>
2023-05-20 09:11:32 +0700

    pussh

---
+++ Nha-Sach-Desktop/UI/taikhoan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nha_Sach_Desktop.UI
+{
+    internal class taikhoan
+    {
+        private string tentaikhoan;
+        
+        
+    }
+}
